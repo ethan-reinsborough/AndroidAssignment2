faeaae6f5e4f60233fb4f68a0aaeeebc6e3c6686
Ethan Reinsborough <ethan.reinsborough@gmail.com>
2021-02-28 22:10:10 -0400

    Fixed About page and overflow menu

--- RickAndMortyQuiz2/app/src/main/java/com/example/rickandmortyquiz/Title.kt
+++ RickAndMortyQuiz2/app/src/main/java/com/example/rickandmortyquiz/Title.kt
@@ -1,15 +1,15 @@
 package com.example.rickandmortyquiz
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.rickandmortyquiz.databinding.FragmentTitleBinding
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 
 class Title : Fragment() {
 
@@ -24,7 +24,17 @@ class Title : Fragment() {
         }
 
         (activity as AppCompatActivity).supportActionBar?.title = getString(R.string.app_name)
-
+        setHasOptionsMenu(true)
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu_main,menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item, requireView().findNavController())
+                || super.onOptionsItemSelected(item)
     }
 }
